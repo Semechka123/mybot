59d919080fc676ad1113d55843599f3fc1bfc5bf
Semechka123 <kondratevaru78@gmail.com>
2021-10-22 22:07:46 +0300

    add datetime

--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@ import sqlite3
 
 import telebot
 
+import datetime
+
 with open("token.txt") as f:
     token=f.readline() 
     
 bot = telebot.TeleBot(token)
 
-def db_table_val(user_id: int, user_name: str, user_surname: str, username: str):
+def user_write(user_id: int, user_name: str, user_surname: str, username: str):
     with sqlite3.connect('database.db') as connection:
         
         try:
@@ -16,7 +18,19 @@ def db_table_val(user_id: int, user_name: str, user_surname: str, username: str)
         except(sqlite3.IntegrityError):
             isold = True 
         return isold
+
         
+def alert_write(user: int, datetime: str, taskname: str):
+    with sqlite3.connect('database.db') as connection:
+             
+        try:
+            connection.execute('INSERT INTO alert (user, datetime, taskname) VALUES (?, ?, ?)', (user, datetime, taskname))
+            query_flag = True
+        except:
+            query_flag = False
+        return query_flag
+
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 
@@ -24,8 +38,9 @@ def get_text_messages(message):
     us_name = message.from_user.first_name
     us_sname = message.from_user.last_name
     username = message.from_user.username
+
 		
-    isold=db_table_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
+    isold=user_write(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
 
     if message.text.lower() == "привет":
         if isold: 
@@ -36,6 +51,26 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Напиши привет")
     elif message.text.lower() == "пока": 
         bot.send_message(message.from_user.id, "Я буду скучать, {}!".format(us_name))
+    elif " ".join(message.text.lower().split()[:2]) == "напомни мне":
+        try:
+            words = message.text.lower().split()
+            date = words[2]
+            format_date = "%d.%m.%Y"
+            time = words[4]
+            format_time = "%H:%M"
+            taskname = " ".join(words[5:])
+            date_v = datetime.datetime.strptime(date, format_date) 
+            time_v = datetime.datetime.strptime(time, format_time).time()
+            date_time = datetime.datetime.combine(date_v, time_v)
+            assert date_time>datetime.datetime.now()
+            dt = str(date_time)
+            result = alert_write(us_id, dt, taskname)
+        except(IndexError, ValueError, AssertionError):
+            result = False
+        if result:
+            bot.send_message(message.from_user.id, "Ок")
+        else:
+            bot.send_message(message.from_user.id, "Формат напоминания: напомни мне 16.10.2021 в 08:00 сходить в школу")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
--- test.py
+++ test.py
@@ -1,7 +1,24 @@
-message = "напомни мне 16.10.2021 в 8:00 сходить в школу, завтра"
-words = message.split()
-date = words[2]
-time = words[4]
-taskname = " ".join(words[5:])
-remind_flag = " ".join(words[:2])
-print(remind_flag, date, time, taskname)
+import datetime as dt
+import sqlite3
+def test(): 
+    message = "напомни мне 16.10.2021 в 8:00 сходить в школу, завтра"
+    words = message.split()
+    date = words[2]
+    time = words[4]
+    taskname = " ".join(words[5:])
+    remind_flag = " ".join(words[:2])
+    print(remind_flag, date, time, taskname)
+
+
+
+def db_table_val(user: int, date: str, taskname: str):
+    with sqlite3.connect('database.db') as connection:
+        connection.execute('INSERT INTO alert (user, date, taskname) VALUES (?, ?, ?)', (user, date, taskname))
+
+
+def alert_read():
+    current_dt = dt.datetime.now()
+    with sqlite3.connect('database.db') as connection:
+        alerts = connection.execute('select * from alert where datetime>?', (str(current_dt),))
+        print(list(alerts))
+alert_read()
